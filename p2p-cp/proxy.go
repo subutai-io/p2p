4aa540617d12f07daac51e1bfa2984682030b947
Mikhail Savochkin <msavochkin@critical-factor.com>
2016-02-01 23:47:17 +0600

    Traffic forwarding simplified

--- p2p-cp/proxy.go
+++ p2p-cp/proxy.go
@@ -28,8 +28,10 @@ type Tunnel struct {
 	Peer1      *net.UDPAddr
 	Peer2      *net.UDPAddr
 	UniqueHash string
+	PingFails  int
 	PingFails1 int
 	PingFails2 int
+	Endpoint   *net.UDPAddr
 }
 
 func (p *Proxy) Initialize(target string) {
@@ -60,6 +62,66 @@ func (p *Proxy) GenerateHash() string {
 }
 
 func (p *Proxy) HandleMessage(count int, src_addr *net.UDPAddr, err error, rcv_bytes []byte) {
+	if err != nil {
+		log.Log(log.ERROR, "P2P Message Handle: %v", err)
+		return
+	}
+
+	buf := make([]byte, count)
+	copy(buf[:], rcv_bytes[:])
+
+	msg, des_err := udpcs.P2PMessageFromBytes(buf)
+	if des_err != nil {
+		log.Log(log.ERROR, "P2PMessageFromBytes error: %v", des_err)
+		return
+	}
+	var msgType commons.MSG_TYPE = commons.MSG_TYPE(msg.Header.Type)
+	if msgType == commons.MT_PROXY {
+		// MT_PROXY indicates that peer (src_addr) can't connect to another peer (msg.data)
+		data := string(msg.Data)
+		var responseId int = -1
+		for id, tun := range p.Tunnels {
+			if tun.Endpoint.String() == data {
+				responseId = int(id)
+			}
+		}
+		if responseId == -1 {
+			log.Log(log.DEBUG, "Making new tunnel")
+			var t Tunnel
+			t.Endpoint, _ = net.ResolveUDPAddr("udp", data)
+			t.PingFails = 0
+			for i := 1; i < len(p.Tunnels)+2; i++ {
+				_, exists := p.Tunnels[uint16(i)]
+				if !exists {
+					p.Tunnels[uint16(i)] = t
+					responseId = i
+					break
+				}
+			}
+		}
+		msg := udpcs.CreateProxyP2PMessage(responseId, data, 0)
+		p.UDPServer.SendMessage(msg, src_addr)
+		p.DHTClient.ReportControlPeerLoad(len(p.Tunnels))
+	} else if msgType == commons.MT_PING {
+		for key, tun := range p.Tunnels {
+			if tun.Peer1.String() == src_addr.String() {
+				tun.PingFails1 = 0
+			} else if tun.Peer2.String() == src_addr.String() {
+				tun.PingFails2 = 0
+			}
+			p.Tunnels[key] = tun
+		}
+	} else {
+		tunnel, exists := p.Tunnels[msg.Header.ProxyId]
+		if !exists {
+			log.Log(log.DEBUG, "Proxy %d is not registered", msg.Header.ProxyId)
+			return
+		}
+		p.UDPServer.SendMessage(msg, tunnel.Endpoint)
+	}
+}
+
+func (p *Proxy) HandleMessageOld(count int, src_addr *net.UDPAddr, err error, rcv_bytes []byte) {
 	if err != nil {
 		log.Log(log.ERROR, "P2P Message Handle: %v", err)
 		return
