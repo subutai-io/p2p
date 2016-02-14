63bc4a55afe1c842bf92249fb64dcdd184ae5b73
Mikhail Savochkin <msavochkin@critical-factor.com>
2016-02-15 00:00:01 +0600

    Working on a windows port

--- lib/tuntap_windows.go
+++ lib/tuntap_windows.go
@@ -2,12 +2,51 @@
 package ptp
 
 import (
+	"fmt"
+	"github.com/lxn/win"
 	"os"
+	"syscall"
 )
 
-var flagTruncated = 0
+const (
+	NETWORK_CONNECTIONS_KEY string = "SYSTEM\\CurrentControlSet\\Control\\Network\\{4D36E972-E325-11CE-BFC1-08002BE10318}"
+	ADAPTER_KEY             string = "SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}"
+	DSIZE                   int    = 72
+)
 
-func createInterface(f *os.File, ifPattern string, kind DevKind) (string, error) {
+func InitTuntap() int {
+	var root win.HKEY
+	rootpath, _ := syscall.UTF16PtrFromString(NETWORK_CONNECTIONS_KEY)
+	ret := win.RegOpenKeyEx(win.HKEY_LOCAL_MACHINE, rootpath, 0, win.KEY_READ, &root)
+	if ret != 0 {
+		return ret
+	}
+	var (
+		name_length  uint32 = DSIZE
+		key_type     uint32
+		lpDataLength uint32 = DSIZE
+		zero_unit    uint32 = 0
+	)
+	name := make([]uint16, DSIZE)
+	lpData := make([]byte, DSIZE)
+
+	ret = win.RegEnumValue(root, zero_unit, &name[0], &name_length, nil, &key_type, &lpData[0], &lpDataLength)
+	fmt.Printf("Execution result is: %d", ret)
+
+	fmt.Printf("lpDataLength: %d\n", lpDataLength)
+	fmt.Printf("name: %d\n", name)
+	fmt.Printf("lpData: %s\n", string(lpData))
+
+	win.RegCloseKey(root)
+	return 0
+}
+
+func openDevice(ifPattern string) (*os.File, error) {
+	file, err := os.OpenFile("/dev/net/tun", os.O_RDWR, 0)
+	return file, err
+}
+
+func createInterface(file *os.File, ifPattern string, kind DevKind, meta bool) (string, error) {
 	panic("TUN/TAP functionality is not supported on this platform")
 }
 
