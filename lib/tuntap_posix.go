bb7491a1d501595373aa0c411e30179c3f92bb29
Mikhail Savochkin <msavochkin@critical-factor.com>
2016-02-23 01:36:08 +0600

    Build fixes: removed unwanted imports

--- lib/tuntap_posix.go
+++ lib/tuntap_posix.go
@@ -7,7 +7,6 @@ import (
 	"io"
 	"os"
 	"os/user"
-	"unsafe"
 )
 
 type Interface struct {
@@ -26,7 +25,7 @@ func (t *Interface) ReadPacket() (*Packet, error) {
 	var pkt *Packet
 	pkt = &Packet{Packet: buf[0:n]}
 	pkt.Protocol = int(binary.BigEndian.Uint16(buf[12:14]))
-	flags := int(*(*uint16)(unsafe.Pointer(&buf[0])))
+	//flags := int(*(*uint16)(unsafe.Pointer(&buf[0])))
 	/*if flags&flagTruncated != 0 {
 		pkt.Truncated = true
 	}*/
