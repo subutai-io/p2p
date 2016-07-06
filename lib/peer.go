61bd0fabd8434fb73370dbd7a33c9a57fbe52a4e
Mikhail Savochkin <msavochkin@critical-factor.com>
2016-07-06 18:46:47 +0600

    Minor changes

--- lib/peer.go
+++ lib/peer.go
@@ -192,7 +192,6 @@ func (np *NetworkPeer) StateWaitingForwarder(ptpc *PTPCloud) error {
 	if np.ProxyRequests >= 3 {
 		Log(INFO, "We've failed to receive any proxies within this period")
 		np.State = P_INIT
-		np.KnownIPs = np.KnownIPs[:0]
 		ptpc.Dht.CleanForwarderBlacklist()
 		np.ProxyRequests = 0
 		return nil
