047fa138e6fc0cde3d58d04880ca4374b23c86c3
Mikhail Savochkin <crioto@users.noreply.github.com>
2018-01-30 16:29:49 +0600

    P2P debug now displays uptime

--- debug.go
+++ debug.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"net/http"
 	"os"
-	"os/signal"
 	"runtime"
-	"runtime/pprof"
+	"time"
 
-	"github.com/ccding/go-stun/stun"
 	ptp "github.com/subutai-io/p2p/lib"
 )
 
@@ -44,63 +41,6 @@ func CommandDebug(restPort int) {
 	os.Exit(out.Code)
 }
 
-// ExecDaemon starts P2P daemon
-func ExecDaemon(port int, sFile, profiling, syslog string) {
-	if syslog != "" {
-		ptp.SetSyslogSocket(syslog)
-	}
-	StartProfiling(profiling)
-	go ptp.InitPlatform()
-	ptp.InitErrors()
-
-	if !ptp.CheckPermissions() {
-		os.Exit(1)
-	}
-
-	ReadyToServe = false
-	proc := new(Daemon)
-	proc.Initialize(sFile)
-	setupRESTHandlers(port, proc)
-
-	ptp.Log(ptp.Info, "Determining outbound IP")
-	nat, host, err := stun.NewClient().Discover()
-	if err != nil {
-		ptp.Log(ptp.Error, "Failed to discover outbound IP: %s", err)
-		OutboundIP = nil
-	} else {
-		OutboundIP = net.ParseIP(host.IP())
-		ptp.Log(ptp.Info, "Public IP is %s. %s", OutboundIP.String(), nat)
-	}
-
-	if sFile != "" {
-		ptp.Log(ptp.Info, "Restore file provided")
-		// Try to restore from provided file
-		instances, err := proc.Instances.LoadInstances(proc.SaveFile)
-		if err != nil {
-			ptp.Log(ptp.Error, "Failed to load instances: %v", err)
-		} else {
-			ptp.Log(ptp.Info, "%d instances were loaded from file", len(instances))
-			for _, inst := range instances {
-				proc.run(&inst, new(Response))
-			}
-		}
-	}
-
-	ReadyToServe = true
-
-	SignalChannel = make(chan os.Signal, 1)
-	signal.Notify(SignalChannel, os.Interrupt)
-
-	go func() {
-		for sig := range SignalChannel {
-			fmt.Println("Received signal: ", sig)
-			pprof.StopCPUProfile()
-			os.Exit(0)
-		}
-	}()
-	select {}
-}
-
 func (d *Daemon) execRESTDebug(w http.ResponseWriter, r *http.Request) {
 	if !ReadyToServe {
 		resp, _ := getResponse(105, "P2P Daemon is in initialization state")
@@ -126,9 +66,8 @@ func (d *Daemon) execRESTDebug(w http.ResponseWriter, r *http.Request) {
 
 // Debug output debug information
 func (p *Daemon) Debug(args *Args, resp *Response) error {
-	ptp.Log(ptp.Info, "Preparing Debug output")
-	resp.Output = "DEBUG INFO:\n"
 	resp.Output = fmt.Sprintf("Version: %s Build: %s\n", AppVersion, BuildID)
+	resp.Output += fmt.Sprintf("Uptime: %d h %d m %d s\n", int(time.Since(StartTime).Hours()), int(time.Since(StartTime).Minutes()), int(time.Since(StartTime).Seconds()))
 	resp.Output += fmt.Sprintf("Number of gouroutines: %d\n", runtime.NumGoroutine())
 	resp.Output += fmt.Sprintf("Instances information:\n")
 	instances := p.Instances.Get()
