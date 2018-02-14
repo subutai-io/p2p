bc24a69c3beb598a885fa8990251d725073df3af
baytulakova <abaytulakova@optimal-dynamics.com>
2018-02-14 17:39:09 +0600

    add tests of proxy_test.go

--- lib/proxy_test.go
+++ lib/proxy_test.go
@@ -2,6 +2,7 @@ package ptp
 
 import (
 	"net"
+	"reflect"
 	"testing"
 )
 
@@ -19,5 +20,120 @@ func TestClose(t *testing.T) {
 
 	if d.Addr != nil && d.Status != 2 && d.Endpoint != nil {
 		t.Error("Close Error")
+	}
+}
+
+func TestOperate(t *testing.T) {
+	p := new(ProxyManager)
+	prsrv := new(proxyServer)
+	p.init()
+	p.proxies["1"] = prsrv
+	oper := OperateUpdate
+	p.operate(oper, "2", prsrv)
+	for i := 0; i < len(p.proxies); i++ {
+		_, exists := p.proxies["2"]
+		if !exists {
+			t.Error("Error in update operation")
+		}
+	}
+	oper2 := OperateDelete
+	p.operate(oper2, "1", prsrv)
+	for i := 0; i < len(p.proxies); i++ {
+		_, exist := p.proxies["1"]
+		if exist {
+			t.Error("Error in delete operation")
+		}
+	}
+}
+
+func TestGet2(t *testing.T) {
+	p := new(ProxyManager)
+	psrv1 := new(proxyServer)
+	psrv1.Addr = new(net.UDPAddr)
+	psrv2 := new(proxyServer)
+	psrv2.Addr = new(net.UDPAddr)
+	p.init()
+	p.proxies["1"] = psrv1
+	p.proxies["2"] = psrv2
+
+	get := p.get()
+	if !reflect.DeepEqual(get, p.proxies) {
+		t.Error("Error. Return wrong values")
+	}
+}
+
+func TestNew(t *testing.T) {
+	p := new(ProxyManager)
+	prsrv1 := new(proxyServer)
+	prsrv2 := new(proxyServer)
+	prsrv1.Endpoint, _ = net.ResolveUDPAddr("24", "192.168.1.1")
+	prsrv2.Endpoint, _ = net.ResolveUDPAddr("24", "192.168.1.2")
+	p.init()
+	p.proxies["1"] = prsrv1
+	p.proxies["2"] = prsrv2
+
+	endpoint, _ := net.ResolveUDPAddr("udp", "192.168.1.1")
+	err := p.new(endpoint)
+	if err != nil {
+		t.Error("Error")
+	}
+}
+
+func TestCheck(t *testing.T) {
+	p := new(ProxyManager)
+	prsrv1 := new(proxyServer)
+	prsrv2 := new(proxyServer)
+	prsrv1.Status = proxyConnecting
+	prsrv2.Status = proxyActive
+	p.init()
+	p.proxies["10"] = prsrv1
+	p.proxies["11"] = prsrv2
+	p.check()
+	_, exists := p.proxies["10"]
+	if prsrv1.Addr != nil && prsrv1.Endpoint != nil && prsrv1.Status != proxyDisconnected && !exists {
+		t.Error("Error")
+	}
+	if prsrv2.Addr != nil && prsrv1.Endpoint != nil && prsrv1.Status != proxyDisconnected && !exists {
+		t.Error("Error")
+	}
+}
+
+func TestTouch(t *testing.T) {
+	p := new(ProxyManager)
+	prsvr1 := new(proxyServer)
+	prsvr2 := new(proxyServer)
+	p.init()
+	p.proxies["100"] = prsvr1
+	p.proxies["101"] = prsvr2
+
+	id1 := "100"
+	get := p.touch(id1)
+	if !get {
+		t.Error("Error")
+	}
+	id2 := "0"
+	get2 := p.touch(id2)
+	if get2 {
+		t.Error("Error. ProxyId is not exists")
+	}
+}
+
+func TestActivate(t *testing.T) {
+	p := new(ProxyManager)
+	endpoint, _ := net.ResolveUDPAddr("24", "192.168.1.1")
+	prsrv1 := new(proxyServer)
+	prsrv1.Status = proxyConnecting
+	prsrv2 := new(proxyServer)
+	p.init()
+	p.proxies["5"] = prsrv1
+	p.proxies["6"] = prsrv2
+
+	get := p.activate("6", endpoint)
+	if !get {
+		t.Error("Error")
+	}
+	get2 := p.activate("0", endpoint)
+	if get2 {
+		t.Error("Error")
 	}
 }
