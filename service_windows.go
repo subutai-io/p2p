94f0e9cb34a8c736bd035c1a9fedae00331a3efb
Azret Kenzhaliev <azret.kenzhaliev@gmail.com>
2018-04-30 15:28:11 +0600

    Update service_windows.go
    * Fixed Error 1067 with stopping the Subutai Windows Service

--- service_windows.go
+++ service_windows.go
@@ -17,7 +17,6 @@ func (m *P2PService) Execute(args []string, r <-chan svc.ChangeRequest, changes 
 	go ExecDaemon(52523, DefaultDHT, "", "", "")
 	const cmdsAccepted = svc.AcceptStop | svc.AcceptShutdown | svc.AcceptPauseAndContinue
 	changes <- svc.Status{State: svc.StartPending}
-
 	changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
 loop:
 	for {
@@ -26,14 +25,15 @@ loop:
 			switch c.Cmd {
 			case svc.Interrogate:
 				changes <- c.CurrentStatus
-				changes <- c.CurrentStatus
 			case svc.Stop, svc.Shutdown:
+				changes <- svc.Status{State: svc.StopPending}
 				changes <- svc.Status{State: svc.Stopped, Accepts: cmdsAccepted}
-				SignalChannel <- os.Interrupt
 				break loop
 			case svc.Pause:
+				changes <- svc.Status{State: svc.PausePending}
 				changes <- svc.Status{State: svc.Paused, Accepts: cmdsAccepted}
 			case svc.Continue:
+				changes <- svc.Status{State: svc.ContinuePending}
 				changes <- svc.Status{State: svc.Running, Accepts: cmdsAccepted}
 			default:
 				ptp.Log(ptp.Error, "Unexpected control request #%d", c)
