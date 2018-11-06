2db93becca577546c381ce2dd39248b97eb9aa3b
Mikhail Savochkin <crioto@users.noreply.github.com>
2018-11-06 17:56:26 +0600

    Added default conf values for each os

---
+++ lib/conf_linux.go
@@ -0,0 +1,12 @@
+// +build linux
+
+package ptp
+
+// Platform specific defaults
+const (
+	DefaultIPTool  = "/sbin/ip" // Default network interface configuration tool for Darwin OS
+	DefaultTAPTool = ""         // Default path to TAP configuration tool on Windows OS
+	DefaultINFFile = ""         // Default path to TAP INF file used by Windows OS
+	DefaultMTU     = 1500       // Default MTU value
+	DefaultPMTU    = false      // Default PMTU switch
+)
---
+++ lib/conf_windows.go
@@ -0,0 +1,12 @@
+// +build windows
+
+package ptp
+
+// Platform specific defaults
+const (
+	DefaultIPTool  = "netsh.exe"                                            // Default network interface configuration tool for Darwin OS
+	DefaultTAPTool = "C:\\Program Files\\TAP-Windows\\bin\\tapinstall.exe"  // Default path to TAP configuration tool on Windows OS
+	DefaultINFFile = "C:\\Program Files\\TAP-Windows\\driver\\OemVista.inf" // Default path to TAP INF file used by Windows OS
+	DefaultMTU     = 1500                                                   // Default MTU value
+	DefaultPMTU    = false                                                  // Default PMTU switch
+)
